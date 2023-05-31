689b362bee81461e3439d7e61ab1dea04ed187fd
WENHUAN22 <zengwenhuan@gmail.com>
2023-05-31 16:21:43 +0000

    add file

--- code/main.py
+++ code/main.py
@@ -1,9 +1,9 @@
 import sys
 import os
-#sys.path.append('/home/ubuntu/project/dna_methy/github/mulan-methyl')
-#os.chdir('/home/ubuntu/project/dna_methy/github/mulan-methyl')
-curPath = os.path.abspath(os.path.dirname(__file__))
-sys.path.append(os.path.abspath(curPath))
+sys.path.append('/home/ubuntu/project/dna_methy/github/mulan-methyl/code')
+os.chdir('/home/ubuntu/project/dna_methy/github/mulan-methyl/code')
+#curPath = os.path.abspath(os.path.dirname(__file__))
+#sys.path.append(os.path.abspath(curPath))
 import run_finetune
 import data_processor
 import run_evaluate
@@ -15,16 +15,20 @@ import torch
 def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_file', default=None, required=True, help='the file path of input data')
-    parser.add_argument('--data_type', default=None, required=False, help='the type(suffix) of input data, txt/tsv/fasta')
     # prediction
     parser.add_argument('--prediction', action='store_true', help='True if conduct prediction (after have finetuned models)')
     parser.add_argument('--species', default=None, required=False, help='species of input data, in full format if self define by user (s__Homo sapiens)')
-    parser.add_argument('--custom_species', action='store_true', required=False, help='if false then the taxonomic lineage of data belongs to iDNA-MS, Presence means True')
     parser.add_argument('--methyl_type', default=None, required=True, help='methylation type, 6mA, 4mC or 5hmC')
     parser.add_argument('--developer', action='store_true', required=False, help='True for developer testing, the uploaded data has label, Presence means True')
     parser.add_argument('--output_file', default=None, required=False, help='the file path of generated result')
     parser.add_argument('--custom_length', action='store_true', required=False, help='True if length of sample is longer than 41')
     parser.add_argument('--do_visualize', action='store_true', required=False, help='True if conduct the computation of importance score')
+    # data processing
+    parser.add_argument('--data_proc', action='store_true', help='process DNA sequence to sentence')
+    parser.add_argument('--data_type', default=None, required=False, help='the type(suffix) of input data, txt/tsv/fasta')
+    parser.add_argument('--labelled', action='store_true', help='true if dataset has label')
+    parser.add_argument('--custom_species', action='store_true', required=False, help='False if taxonomic lineage of data belongs to iDNA-MS, Presence means True')
+    parser.add_argument('--data_output_dir', default=False, required=False, help='Directory of processed data')
     # fine-tuning
     parser.add_argument('--finetune', action='store_true', help='True if fine tuning models')
     parser.add_argument('--model_list', default=False, required=False, nargs='+', help='the model name for fine-tuning')
@@ -59,6 +63,12 @@ def main():
     elif args.finetune:
         func = run_finetune.Finetune()
         func._run_finetune(args.input_file, args.model_list, args.finetuned_output_dir, args.methyl_type)
+    elif args.data_proc:
+        func_ = data_processor.DataProcesser()
+        processed_df = func_.data_loader(args.input_file, args.data_type, args.custom_species, args.labelled)
+        processed_df.to_csv(f'{args.data_output_dir}/processed.tsv', sep='\t', index=False)
+
+
 
 
 
